a3214d12295aa92908836aaad6e5ddd2720bffc5
Erickpabi <72472923+Erickpabi@users.noreply.github.com>
2022-07-20 18:16:26 +0000

    ss

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -9,10 +9,10 @@ function changeIcon(anchor) {
   function changeIcon2() {
     let element = document.getElementById("expandIcon")
     console.log(element.innerText)
-    if(element.innerText === 'expand_more'){
-        element.innerText = 'expand_less'
-    } else {
+    if(element.innerText === 'expand_less'){
         element.innerText = 'expand_more'
+    } else {
+        element.innerText = 'expand_less'
     }
     
   }
