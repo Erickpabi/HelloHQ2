3d4585bd2e14d8abc068d882fafd3fe32da7c119
Erickpabi <72472923+Erickpabi@users.noreply.github.com>
2022-07-20 22:11:15 +0000

    com

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,12 +1,4 @@
-function changeIcon(anchor) {
-    var icon = anchor.querySelector("span");
-    icon.classList.toggle('expand-less');
-    icon.classList.toggle('expand-more');
-  
- 
-  }
-
-  function changeIcon2() {
+function changeIcon2() {
     let element = document.getElementById("expandIcon")
     console.log(element.innerText)
     if(element.innerText === 'expand_less'){
